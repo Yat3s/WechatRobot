cedfa9a6154815d40f6379155a0e5b9d48db7ec2
Yat3s <4645643@qq.com>
2017-04-24 15:28:18 +0800

    Added group name at

--- bot.py
+++ bot.py
@@ -11,7 +11,8 @@ class TulingWXBot(WXBot):
 
         self.tuling_key = ""
         self.robot_switch = True
-        self.adminName = u"Yat3s"
+        self.adminName = u'Yat3s'
+        self.robotName = u'Aran'
         self.DEBUG = True
 
         try:
@@ -49,25 +50,24 @@ class TulingWXBot(WXBot):
             return u"知道啦"
 
 
-    def hand_admin_msg(self, msg):
-        content = msg['content']['data']
+    def hand_admin_msg(self, msgContent, userId):
         replyMsg = u"欢迎主人，请吩咐!"
         stop_cmd = [u'stop', u'退下', u'走开', u'关闭', u'关掉', u'休息', u'滚开']
         start_cmd = [u'start', u'出来', u'启动', u'工作']
         findCmd = False;
         if self.robot_switch:
             for idx in stop_cmd:
-                if idx == content:
+                if idx == msgContent:
                     self.robot_switch = False
-                    self.send_msg_by_uid(u'爸爸我滚去睡觉了', msg['user']['id'])
+                    self.send_msg_by_uid(u'叶爸爸我滚去睡觉了', userId)
                     findCmd = True;
         else:
             for idx in start_cmd:
-                if idx == content:
+                if idx == msgContent:
                     self.robot_switch = True
-                    self.send_msg_by_uid(u'爸爸我起来了', msg['user']['id'])
+                    self.send_msg_by_uid(u'叶爸爸我起来了', userId)
         if not findCmd:
-            self.send_msg_by_uid(replyMsg, msg['user']['id'])
+            self.send_msg_by_uid(u'叶爸爸，' + self.tuling_auto_reply(userId, msgContent), userId)
 
 
     def handle_msg_all(self, msg):
@@ -86,7 +86,7 @@ class TulingWXBot(WXBot):
         if not self.robot_switch and username != self.adminName:
             return
         if username == self.adminName: # Process admin command
-            self.hand_admin_msg(msg)
+            self.hand_admin_msg(messgae_content, user_id)
         elif msg['msg_type_id'] == 4 and msg['content']['type'] == 0:  # text message from contact
             self.send_msg_by_uid(self.tuling_auto_reply(msg['user']['id'], msg['content']['data']), msg['user']['id'])
         elif msg['msg_type_id'] == 3 and msg['content']['type'] == 0:  # group text message
@@ -115,19 +115,20 @@ class TulingWXBot(WXBot):
                 for detail in msg['content']['detail']:
                     # if detail['type'] == 'at':
                     for k in my_names:
-                        if my_names[k] and my_names[k] in detail['value']:
+                        if my_names[k] and my_names[k] in detail['value'] or self.robotName in detail['value']:
                             is_at_me = True
                             break
                 if is_at_me:
                     src_name = msg['content']['user']['name']
-                    reply = 'to ' + src_name + ': '
                     if src_name == self.adminName:
-                        reply = u'叶爸爸，'
-                    if msg['content']['type'] == 0:  # text message
-                        reply += self.tuling_auto_reply(msg['content']['user']['id'], msg['content']['desc'])
-                    else:
-                        reply += u"对不起，我刚上二年级，只看得懂字，其他杂七杂八的我都不认识，,,Ծ‸Ծ,,"
-                    self.send_msg_by_uid(reply, msg['user']['id'])
+                        self.hand_admin_msg(msg['content']['desc'], msg['user']['id'])
+                    else :
+                        reply = 'to ' + src_name + ': '
+                        if msg['content']['type'] == 0:  # text message
+                            reply += self.tuling_auto_reply(msg['content']['user']['id'], msg['content']['desc'])
+                        else:
+                            reply += u"对不起，我刚上二年级，只看得懂字，其他杂七杂八的我都不认识，,,Ծ‸Ծ,,"
+                        self.send_msg_by_uid(reply, msg['user']['id'])
 
 
 def main():
